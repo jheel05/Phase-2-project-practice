fa51704aeaba0e1978db97e0a4a6051b6391b0f9
Jheel Soni <jheel.20bcon158@jecrcu.edu.in>
2024-02-13 13:24:52 +0000

    debounce add

---
+++ assets/searchbox.js
@@ -0,0 +1,43 @@
+let timer;
+function debounce(fn,delay){
+  clearTimeout(timer);
+  timer=setTimeout(fn,delay);
+}
+
+function fetchData(query){
+    return fetch('https://dummyjson.com/products/search?q=' + query)
+    .then(response=>response.json())
+    .catch(error=>console.error("Error fetching data:",error));
+}
+
+function displayResults(results)
+{
+  const searchElement=document.getElementById("searchResults");
+  searchResults.innerElement='';
+  const req=results.products;
+  console.log(req);
+  req.forEach(result => {
+    const li = document.createElement('li');
+    li.textContent = result.title;
+    li.addEventListener('click', () => selectSuggestion(result.title));
+    searchElement.appendChild(li);
+  });
+
+}
+
+function selectSuggestion(selectedSuggestion){
+  document.getElementById('search').value
+}
+
+function debouncedSearch(event){
+  const query=event.target.value;
+  debounce(()=>{
+    if(query.trim()!='')
+    {
+      fetchData(query)
+    .then(displayResults);
+    } else{
+      document.getElementById("searchResults").innerHTML='';
+    }
+  },500);
+}
