f246046d5489560c61ac0cacff7c7a5d7d0da070
Jheel Soni <jheel.20bcon158@jecrcu.edu.in>
2024-02-14 09:27:34 +0000

    modification

--- assets/JS/otplogin.js
+++ assets/JS/otplogin.js
@@ -1,33 +1,77 @@
 
-const inputs = document.getElementById("inputs");
+const input = document.querySelectorAll(".input");
+const inputField = document.querySelector(".inputfield");
+const submitButton = document.getElementById("submit");
+let inputCount = 0,
+  finalInput = "";
 
-inputs.addEventListener("input", function (e) {
-	const target = e.target;
-	const val = target.value;
 
-	if (isNaN(val)) {
-		target.value = "";
-		return;
-	}
+const updateInputConfig = (element, disabledStatus) => {
+  element.disabled = disabledStatus;
+  if (!disabledStatus) {
+    element.focus();
+  } else {
+    element.blur();
+  }
+};
 
-	if (val != "") {
-		const next = target.nextElementSibling;
-		if (next) {
-			next.focus();
-		}
-	}
+input.forEach((element) => {
+  element.addEventListener("keyup", (e) => {
+    e.target.value = e.target.value.replace(/[^0-9]/g, "");
+    let { value } = e.target;
+
+    if (value.length == 1) {
+      updateInputConfig(e.target, true);
+      if (inputCount <= 3 && e.key != "Backspace") {
+        finalInput += value;
+        if (inputCount < 3) {
+          updateInputConfig(e.target.nextElementSibling, false);
+        }
+      }
+      inputCount += 1;
+    } else if (value.length == 0 && e.key == "Backspace") {
+      finalInput = finalInput.substring(0, finalInput.length - 1);
+      if (inputCount == 0) {
+        updateInputConfig(e.target, false);
+        return false;
+      }
+      updateInputConfig(e.target, true);
+      e.target.previousElementSibling.value = "";
+      updateInputConfig(e.target.previousElementSibling, false);
+      inputCount -= 1;
+    } else if (value.length > 1) {
+      e.target.value = value.split("")[0];
+    }
+    submitButton.classList.add("hide");
+  });
 });
 
-inputs.addEventListener("keyup", function (e) {
-	const target = e.target;
-	const key = e.key.toLowerCase();
-
-	if (key == "backspace" || key == "delete") {
-		target.value = "";
-		const prev = target.previousElementSibling;
-		if (prev) {
-			prev.focus();
-		}
-		return;
-	}
+window.addEventListener("keyup", (e) => {
+  if (inputCount > 3) {
+    submitButton.classList.remove("hide");
+    submitButton.classList.add("show");
+    if (e.key == "Backspace") {
+      finalInput = finalInput.substring(0, finalInput.length - 1);
+      updateInputConfig(inputField.lastElementChild, false);
+      inputField.lastElementChild.value = "";
+      inputCount -= 1;
+      submitButton.classList.add("hide");
+    }
+  }
 });
+
+const validateOTP = () => {
+	window.location.href = "/../../index.html";
+};
+
+
+const startInput = () => {
+  inputCount = 0;
+  finalInput = "";
+  input.forEach((element) => {
+    element.value = "";
+  });
+  updateInputConfig(inputField.firstElementChild, false);
+};
+
+window.onload = startInput();
